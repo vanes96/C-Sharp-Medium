8e17d0e24491d1aa3293e4a41e94ed5175af1c1e
Chilikin Ivan <vanesdacha@mail.ru>
2019-09-01 21:49:25 +0300

    Code style fix

---
+++ Task2/Object.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Task2
+{
+    class Object
+    {
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public void MoveRandomly()
+        {
+            Random random = new Random();
+
+            X += random.Next(-1, 1);
+            if (X < 0)
+                X = 0;
+
+            Y += random.Next(-1, 1);
+            if (Y < 0)
+                Y = 0;
+        }
+        public Object(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ Task2/ObjectList.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace Task2
+{
+    class ObjectList
+    {
+        private readonly List<Object> _objects;
+
+        public ObjectList(Point[] points)
+        {
+            _objects = new List<Object>();
+
+            foreach (var point in points)
+                _objects.Add(new Object(point.X, point.Y));
+        }
+
+        public bool IsObjectAlive(int index)
+        {
+            for (int i = 0; i < _objects.Count; i++)
+                if (i != index && _objects[i].X == _objects[index].X && _objects[i].Y == _objects[index].Y)
+                    return false;
+
+            return true;
+        }
+
+        public void MoveAllObjectsRandomly()
+        {
+            foreach (var obj in _objects)
+                obj.MoveRandomly();
+        }
+
+        public void MoveCursorToAllObjects()
+        {
+            for (int i = 0; i < _objects.Count; i++)
+                if (IsObjectAlive(i))
+                {
+                    Console.SetCursorPosition(_objects[i].X, _objects[i].Y);
+                    Console.Write(i + 1);
+                }
+        }
+    }
+}
