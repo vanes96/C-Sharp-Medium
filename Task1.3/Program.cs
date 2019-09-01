b50b125fda841a83497397fef3a809165d71bd1a
Chilikin Ivan <vanesdacha@mail.ru>
2019-09-01 23:41:20 +0300

    Project creation

---
+++ Task1.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1._3
+{
+    class Bag
+    {
+        public List<Item> Items;
+        public int MaxWeidth;
+
+        public void AddItem(string name, int count)
+        {
+            int currentWeidth = Items.Sum(item => item.Count);
+            Item targetItem = Items.FirstOrDefault(item => item.Name == name);
+
+            if (targetItem == null)
+                throw new InvalidOperationException();
+
+            if (currentWeidth + count > MaxWeidth)
+                throw new InvalidOperationException();
+
+            targetItem.Count += count;
+        }
+    }
+
+    class Item
+    {
+        public int Count;
+        public string Name;
+    }
+}
