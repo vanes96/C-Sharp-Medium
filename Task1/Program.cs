91ee3479e44aafa7b2b67ec8e89b561b69312ecf
Chilikin Ivan <vanesdacha@mail.ru>
2019-09-01 18:06:57 +0300

    Task1 solution

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -10,6 +10,15 @@ namespace Task1
     {
         static void Main(string[] args)
         {
+            // Task1 example
+            UserList users = new UserList(new string[] {"Ivan", "Petr", "Olga", "Anna", "Dariana" }, new uint[] { 500, 300, 450, 200, 220 }, 5);
+
+            User user1 = users.GetUserById(1);
+            User user2 = users.GetUserByName("Petr");
+            var allUsers = users.GetAllUsers();
+            var richUsers = users.GetRicherUsers(300);
+            var poorUsers = users.GetPoorerUsers(300);
+            var rangeUsers = users.GetRangeUsers(250, 460);
         }
     }
 }
---
+++ Task1/User.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class User
+    {
+        public string Name { get; }
+        public uint Id { get; }
+        public uint Salary { get; }
+        public User(uint id, string name, uint salary)
+        {
+            Id = id;
+            Name = name;
+            Salary = salary;
+        }
+    }
+
+    class UserList
+    {
+        private readonly List<User> _users;
+
+        public User GetUserById(uint id)
+        {
+            return _users.Single(u => u.Id == id);
+        }
+        public User GetUserByName(string name)
+        {
+            return _users.Single(u => u.Name == name);
+        }
+        public IReadOnlyCollection<User> GetAllUsers()
+        {
+            return _users;
+        }
+        public IReadOnlyCollection<User> GetRicherUsers(uint n)
+        {
+            return _users.ToList().FindAll(u => u.Salary > n);
+        }
+        public IReadOnlyCollection<User> GetPoorerUsers(uint n)
+        {
+            return _users.ToList().FindAll(u => u.Salary < n);
+        }
+        public IReadOnlyCollection<User> GetRangeUsers(uint n1, uint n2)
+        {
+            return _users.ToList().FindAll(u => u.Salary >= n1 && u.Salary <= n2);
+        }
+        public UserList(string[] names, uint[] salaries, uint count)
+        {
+            _users = new List<User>();
+            for (uint i = 0; i < count; i++)
+                _users.Add(new User(i + 1, names[i], salaries[i]));
+        }
+    }
+}
