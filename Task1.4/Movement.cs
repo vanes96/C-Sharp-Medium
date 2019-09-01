caaf3adb9d56858a1ccf6cff1ac91331fd8a519a
Chilikin Ivan <vanesdacha@mail.ru>
2019-09-01 23:49:48 +0300

    Task1.4 done

---
+++ Task1.4/Movement.cs
@@ -0,0 +1,9 @@
+﻿namespace Task1._4
+{
+    class Movement
+    {
+        public float DirectionX { get; private set; }
+        public float DirectionY { get; private set; }
+        public float Speed { get; private set; }
+    }
+}
---
+++ Task1.4/Weapon.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Task1._4
+{
+    class Weapon
+    {
+        public float Cooldown { get; private set; }
+        public int Damage { get; private set; }
+        public bool IsReloading()
+        {
+            throw new NotImplementedException();
+            // if (Cooldown...)
+        }
+    }
+}
