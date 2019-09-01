b7b8bf3942d319bcebeb90570c2b6d27986aa272
Chilikin Ivan <vanesdacha@mail.ru>
2019-09-01 21:39:22 +0300

    Task4 solution

---
+++ Task4/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Weapon
+    {
+        public float Cooldown { get; private set; }
+        public int Damage { get; private set; }
+        public bool IsReloading()
+        {
+            throw new NotImplementedException();
+            // if (Cooldown...)
+        }
+    }
+
+    class Movement
+    {
+        public float DirectionX { get; private set; }
+        public float DirectionY { get; private set; }
+        public float Speed { get; private set; }
+    }
+
+    class Player
+    {
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public Weapon Weapon { get; private set; }
+        public Movement Movement { get; private set; }
+        public void Move()
+        {
+            //Do move 
+            // Movement.Do()
+        }
+
+        public void Attack()
+        {
+            //attack
+            // If (Weapon.IsReloading()) ...
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
