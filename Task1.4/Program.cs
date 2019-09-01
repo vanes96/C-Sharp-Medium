cf3b5dc5021fbbf1149e88e343916037ed45b053
Chilikin Ivan <vanesdacha@mail.ru>
2019-09-01 23:45:37 +0300

    Project creation

---
+++ Task1.4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1._4
+{
+    class Player
+    {
+        public string Name { get; private set; }
+        public float WeaponCooldown { get; private set; }
+        public float MovementDirectionX { get; private set; }
+        public float MovementDirectionY { get; private set; }
+        public int Age { get; private set; }
+        public int WeaponDamage { get; private set; }
+        public float MovementSpeed { get; private set; }
+
+        public void Move()
+        {
+            //Do move
+        }
+
+        public void Attack()
+        {
+            //attack
+        }
+
+        public bool IsReloading()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
