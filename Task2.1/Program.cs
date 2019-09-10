616e69ea81ba2b019bfc729fda5523336daacfc1
Chilikin Ivan <vanesdacha@mail.ru>
2019-09-10 17:13:45 +0300

    Task2.1 done

--- Task2.1/Program.cs
+++ Task2.1/Program.cs
@@ -1,15 +1,39 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Task2._1
 {
     class Program
     {
-        static void Main(string[] args)
+        class Combat : Human
         {
+            private int _armor;
+
+            public override void TakeDamage(int damage)
+            {
+                _health -= damage - _armor;
+
+                if (_health <= 0)
+                    Die();
+            }
+        }
+
+        class Human
+        {
+            protected int _health;
+            private int _agility;
+
+            protected void Die()
+            {
+                Console.WriteLine("Я умер");
+            }
+
+            public virtual void TakeDamage(int damage)
+            {
+                _health -= damage / _agility;
+
+                if (_health <= 0)
+                    Die();
+            }
         }
     }
 }
