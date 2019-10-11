dd31460d8d7b068955f738fef10000729598f993
Chilikin Ivan <vanesdacha@mail.ru>
2019-10-11 19:41:17 +0300

    Project creation

---
+++ Task3.2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (Console.ReadKey().Key != ConsoleKey.Escape)
+            {
+                
+                string commandNumber = Console.ReadLine();
+
+                switch (commandNumber)
+                {
+                    case "1":
+                        
+                        break;
+                    case "2":
+                        
+                        break;
+                    case "3":
+                        
+                        break;
+                    default:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine($"Команда {commandNumber} не существует!");
+                        Console.ResetColor();
+                        break;
+                }
+            }
+        }
+    }
+}
