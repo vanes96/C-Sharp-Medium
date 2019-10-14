7fa1f5b44ffc829add9670f00cf57d65a6f3ba81
Chilikin Ivan <vanesdacha@mail.ru>
2019-10-14 20:10:33 +0300

    Code has been copied

--- Task3.4/Program.cs
+++ Task3.4/Program.cs
@@ -10,6 +10,53 @@ namespace Task3._4
     {
         static void Main(string[] args)
         {
+            SomeGlobalClass SGC = new SomeGlobalClass();
+            BigData d = new BigData();
+            BigData d2 = new BigData();
+            d = null;
+            d2 = null;
+
+            GC.Collect();
+
+            while (true)
+            {
+
+            }
+        }
+    }
+
+    class SomeGlobalClass
+    {
+        public static SomeGlobalClass Instance;
+        public event Action OnSomething;
+
+        public SomeGlobalClass()
+        {
+            Instance = this;
+        }
+
+        public void DoSomething()
+        {
+            if (OnSomething != null)
+            {
+                OnSomething();
+            }
+        }
+    }
+
+    class BigData
+    {
+        public int[] Data;
+
+        public BigData()
+        {
+            Data = new int[100000];
+            SomeGlobalClass.Instance.OnSomething += EventHandler;
+        }
+
+        public void EventHandler()
+        {
+
         }
     }
 }
