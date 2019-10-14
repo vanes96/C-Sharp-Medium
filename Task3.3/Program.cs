8206e0cdcebf75057f12a61c68b5713f5b928537
Chilikin Ivan <vanesdacha@mail.ru>
2019-10-14 17:53:42 +0300

    SendRequest function was implemented

--- Task3.3/Program.cs
+++ Task3.3/Program.cs
@@ -1,8 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace Task3._3
 {
@@ -10,6 +11,56 @@ namespace Task3._3
     {
         static void Main(string[] args)
         {
+            Uri uri = new Uri("https://yandex.ru/");
+            var jsonData = new Dictionary<string, string>
+            {
+              {"CustomerId", "5"},
+              {"CustomerName", "Pepsi"}
+            };
+
+            HttpContent content = new StringContent(JsonConvert.SerializeObject(jsonData), Encoding.UTF8, "application/json");
+
+            //var task = Task.Run(() => SendURI(uri, "POST", content));
+            var task = Task.Run(() => SendRequest(uri, "GET", content));
+
+            task.Wait();
+
+            Console.WriteLine(task.Result);
+            Console.ReadLine();
         }
+
+        static async Task<string> SendRequest(Uri uri, string method = "GET", HttpContent content = null)
+        {
+            var response = string.Empty;
+            using (var client = new HttpClient())
+            {
+                HttpRequestMessage request = new HttpRequestMessage
+                {
+                    Method = new HttpMethod(method),
+                    RequestUri = uri,
+                    Content = content
+                };
+
+                HttpResponseMessage result = null;
+
+                switch(method)
+                {
+                    case "GET":
+                        result = await client.GetAsync(uri);
+                        if (result.IsSuccessStatusCode)
+                            response = await result.Content.ReadAsStringAsync();
+                        break;
+
+                    case "POST":
+                        result = await client.PostAsync(uri, content);
+                        if (result.IsSuccessStatusCode)
+                            response = result.StatusCode.ToString();
+                        break;
+                }
+            }
+
+            return response;
+        }
+
     }
 }
